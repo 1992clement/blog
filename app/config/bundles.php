54668fbcd30b24f2681e671c5caf9c90fbcefd9c
Clement Trumpff <cltrumpff@gmail.com>
2025-10-25 21:22:22 +0200

    chore(email-verification): add dependencies for email verification : mailer, messenger, verify-email-bundle, redis-messenger

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
