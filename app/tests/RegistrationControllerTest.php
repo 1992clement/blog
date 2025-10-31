4ad666b49fc0f6d916aceea29fe3e0a475c05925
Clement Trumpff <cltrumpff@gmail.com>
2025-10-31 12:52:03 +0100

    feat(registration): implement registration form & registration process

---
+++ app/tests/RegistrationControllerTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegistrationControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+
+    private UserRepository $userRepository;
+
+    private UserPasswordHasherInterface $passwordHasher;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        // Ensure we have a clean database
+        $container = static::getContainer();
+
+        $this->userRepository = $container->get(UserRepository::class);
+        $this->passwordHasher = $container->get(UserPasswordHasherInterface::class);
+
+        /** @var EntityManager $em */
+        $em = $container->get('doctrine')->getManager();
+        foreach ($this->userRepository->findAll() as $user) {
+            $em->remove($user);
+        }
+
+        $em->flush();
+    }
+
+    public function testRegister(): void
+    {
+        $this->client->request('GET', '/register');
+        self::assertResponseIsSuccessful();
+        self::assertPageTitleContains('Register');
+
+        $this->client->submitForm('Register', [
+            'registration_form[username]' => 'Robert',
+            'registration_form[email]' => 'me@example.com',
+            'registration_form[password][first]' => 'password',
+            'registration_form[password][second]' => 'password',
+        ]);
+        // Ensure redirection & user creation
+        self::assertResponseRedirects('/');
+        $users = $this->userRepository->findAll();
+        self::assertCount(1, $users);
+
+        $user = $users[0];
+        $this->assertUserData($user);
+
+        // Ensure the verification email was sent
+        self::assertEmailCount(1);
+        self::assertCount(1, $messages = $this->getMailerMessages());
+        self::assertEmailAddressContains($messages[0], 'from', 'registration@clementtrumpff.com');
+        self::assertEmailAddressContains($messages[0], 'to', 'me@example.com');
+        self::assertEmailTextBodyContains($messages[0], 'This link will expire in 1 hour.');
+
+        // Login the new user
+        $this->client->followRedirect();
+        $this->client->loginUser($user);
+
+        // Get the verification link from the email
+        /** @var TemplatedEmail $templatedEmail */
+        $templatedEmail = $messages[0];
+        $messageBody = $templatedEmail->getHtmlBody();
+        self::assertIsString($messageBody);
+
+        preg_match('#(http://localhost/verify/email.+)">#', $messageBody, $resetLink);
+
+        // "Click" the link and see if the user is verified
+        $this->client->request('GET', $resetLink[1]);
+        $this->client->followRedirect();
+
+        self::assertTrue(static::getContainer()->get(UserRepository::class)->findAll()[0]->isVerified());
+    }
+
+    private function assertUserData(User $user): void
+    {
+        self::assertSame('Robert', $user->getUsername());
+        self::assertSame('me@example.com', $user->getEmail());
+        self::assertTrue($this->passwordHasher->isPasswordValid($user, 'password'));
+        self::assertSame(['ROLE_USER'], $user->getRoles());
+        self::assertEmpty($user->getPresentation());
+        $this->assertEqualsWithDelta(
+            time(),
+            $user->getRegistrationDate()->getTimestamp(),
+            5
+        );
+
+        $this->assertEqualsWithDelta(
+            time(),
+            $user->getLastLoginDate()->getTimestamp(),
+            5
+        );
+        self::assertFalse($user->isVerified());
+    }
+}
