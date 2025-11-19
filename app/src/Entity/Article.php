926a40c914bf8c0b5440f419f95f2d377a292014
Clement Trumpff <cltrumpff@gmail.com>
2025-11-19 11:21:00 +0100

    feat(Article): fetch comments ordered by creationDate DESC

--- app/src/Entity/Article.php
+++ app/src/Entity/Article.php
@@ -36,6 +36,7 @@ class Article
      * @var Collection<int, Comment>
      */
     #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'article', orphanRemoval: true)]
+    #[ORM\OrderBy(['creationDate' => 'DESC'])]
     private Collection $comments;
 
     public function __construct()
